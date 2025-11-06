3e51e7040e1df13d270ee605b9142ece32e120c0
kevinuyphan-lgtm <kevinuyphan@gmail.com>
2025-11-06 20:52:39 +0100

    Update generate_pdf.py

--- generate_pdf.py
+++ generate_pdf.py
@@ -157,5 +157,5 @@ def generate_invoice_pdf(invoice_data):
     pdf.cell(60, 7, "", ln=False, align="L")
     pdf.cell(0, 7, f"KID: {vf.kid}", ln=True, align="R")
 
-    pdf_output = pdf.output(dest="S").encode("latin-1")
-    return BytesIO(pdf_output)
+    pdf_bytes = pdf.output(dest="S")  # returnerer bytes
+    return BytesIO(pdf_bytes)
