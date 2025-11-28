2ce5b5a6cea7956166c0108b0e94a6c0401cd945
kevinuyphan-lgtm <kevinuyphan@gmail.com>
2025-11-28 20:59:50 +0100

    Delete extensions.py

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
-from itsdangerous import URLSafeTimedSerializer
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-migrate = Migrate()
-serializer = None  # Init i create_app med SECRET_KEY
