5da90b1930aba48f651675a71587de3510e55a0e
kevinuyphan-lgtm <kevinuyphan@gmail.com>
2025-11-28 20:59:31 +0100

    Create models.py

---
+++ system/models.py
@@ -0,0 +1,31 @@
+from extensions import db
+from datetime import datetime
+
+class Bruker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    navn = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    faktura_teller = db.Column(db.Integer, default=1000)
+    fakturaer = db.relationship('Faktura', backref='bruker', lazy=True)
+    kunder = db.relationship('Kunde', backref='bruker', lazy=True)
+
+class Kunde(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    navn = db.Column(db.String(150), nullable=False)
+    epost = db.Column(db.String(150))
+    telefon = db.Column(db.String(50))
+    adresse = db.Column(db.String(200))
+    orgnr = db.Column(db.String(50))
+    referanse = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('bruker.id'), nullable=False)
+    firmanavn = db.Column(db.String(150))
+    land = db.Column(db.String(50))
+
+class Faktura(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filnavn = db.Column(db.String(100), nullable=False)
+    dato = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(20), default="utkast")
+    betalt_dato = db.Column(db.DateTime, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('bruker.id'), nullable=False)
