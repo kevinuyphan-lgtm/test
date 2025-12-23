05e61cdf95b837726d7300f4500a091d3744eca9
kevinuyphan-lgtm <kevinuyphan@gmail.com>
2025-12-23 20:14:14 +0100

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -49,7 +49,7 @@ def login():
             session['user'] = user.navn
             session['user_id'] = user.id
             flash("Velkommen tilbake!", "success")
-            return redirect(url_for("faktura.sendte"))
+            return redirect(url_for("auth.index"))
         flash("Feil epost eller passord", "error")
         return redirect(url_for("auth.login"))
     return render_template("users/login.html")
