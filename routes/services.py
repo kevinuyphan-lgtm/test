7c42d0796236716fd49339e705462e0cf4a5e83a
kevinuyphan-lgtm <kevinuyphan@gmail.com>
2026-02-12 20:48:21 +0100

    Update services.py

--- routes/services.py
+++ routes/services.py
@@ -28,3 +28,40 @@ def tjenester():
     user = Bruker.query.get(uid)
     kunder = Kunde.query.filter_by(user_id=user.id).all()
     return render_template("faktura_tjeneste/tjenester.html", kunder=kunder)
+
+from flask import request, jsonify
+from system.extensions import db
+from system.models import Sender
+
+
+@services_bp.route("/get-sender", methods=["GET"])
+def get_sender():
+    sender = Sender.query.first()
+    if not sender:
+        return jsonify({"exists": False})
+    
+    return jsonify({
+        "exists": True,
+        "firmanavn": sender.firmanavn,
+        "orgnr": sender.orgnr,
+        "adresse": sender.adresse
+    })
+
+
+@services_bp.route("/save-sender", methods=["POST"])
+def save_sender():
+    data = request.get_json()
+
+    sender = Sender.query.first()
+
+    if not sender:
+        sender = Sender()
+        db.session.add(sender)
+
+    sender.firmanavn = data.get("firmanavn")
+    sender.orgnr = data.get("orgnr")
+    sender.adresse = data.get("adresse")
+
+    db.session.commit()
+
+    return jsonify({"success": True})
