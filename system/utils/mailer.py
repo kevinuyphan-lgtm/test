b0e014afa0e4951b2b683ac934090d77cc6e85d9
kevinuyphan-lgtm <kevinuyphan@gmail.com>
2026-02-24 22:34:59 +0100

    Update mailer.py

--- system/utils/mailer.py
+++ system/utils/mailer.py
@@ -2,10 +2,29 @@ import smtplib
 import os
 from email.message import EmailMessage
 from system.config import Config
+from itsdangerous import URLSafeTimedSerializer
+from flask import current_app
+
+
+def generate_reset_token(email):
+    serializer = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])
+    return serializer.dumps(email, salt="password-reset-salt")
+
+
+def verify_reset_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])
+    try:
+        email = serializer.loads(
+            token,
+            salt="password-reset-salt",
+            max_age=expiration
+        )
+    except Exception:
+        return None
+    return email
 
 
 def send_email(subject, to_email, html_content, attachments=None):
-
     msg = EmailMessage()
     msg["Subject"] = subject
     msg["From"] = Config.MAIL_USERNAME
@@ -27,7 +46,6 @@ def send_email(subject, to_email, html_content, attachments=None):
                 filename=file_name,
             )
 
-    # 👇 SMTP SKAL VÆRE INNI FUNKSJONEN
     with smtplib.SMTP_SSL(Config.MAIL_SERVER, Config.MAIL_PORT) as server:
         server.login(Config.MAIL_USERNAME, Config.MAIL_PASSWORD)
         server.send_message(msg)
